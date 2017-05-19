cbea3ab7ca048f7a7b840025727bd0025165613e
gderick-1 <godsonderick@gmail.com>
2017-05-20 01:17:12 +0300

    layout design

--- resources/views/includes/headerSessions.blade.php
+++ resources/views/includes/headerSessions.blade.php
@@ -1,18 +1,28 @@
 
 
-<section class=" leftbar col-md-3">
-    <img  src="{{URL::to('images/noavatar.jpg')}}">
+<section class="leftbar col-md-3">
+    {{--<img  src="{{URL::to('images/noavatar.jpg')}}">
     <div class="dr_title  panel panel-success">
         <h3>Dr. FYP</h3>
         <h3>Title</h3>
+    </div>--}}
+    <div>
+        <form class="well form-horizontal">
+            <fieldset style="align-content: center">
+                <legend><h1 style="text-align: center;">Today</h1></legend>
+            </fieldset>
+        </form>
     </div>
-    {{--side bar links --}}
-    <ul class="leftbar  nav nav-pills nav-stacked">
-        <li class="active"><a href="/">Home</a></li>
-        <li><a href="referral">eReferrals</a></li>
-        <li><a href="view" >View referrals</a></li>
-        <li><a href="admin">Admin</a></li>
-        <li><a href="#about">About Us</a></li>
-    </ul>
+    <div>
+        {{--side bar links--}}
+        <ul class="nav nav-pills nav-stacked">
+            <li><a class="small text-uppercase text-letterspacing" href="/">Home</a></li><br/>
+            <li><a class="small text-uppercase text-letterspacing" href="referral">eReferrals</a></li><br/>
+            <li><a class="small text-uppercase text-letterspacing" href="view" >View referrals</a><br/>
+            <li><a class="small text-uppercase text-letterspacing" href="admin">Admin</a></li><br/>
+            <li><a class="small text-uppercase text-letterspacing" href="#about">About Us</a></li><br/>
+        </ul>
+    </div>
+
 </section>
 
