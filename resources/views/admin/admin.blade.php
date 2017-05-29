bbc19e065518f7cf89fc86f824efeb40278c4c63
gderick-1 <godsonderick@gmail.com>
2017-05-29 11:37:55 +0300

    admin dashboard modified

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -9,19 +9,93 @@
                 <div class=" leftbar col-md-3">
                    <!-- side bar links-->
                     <ul class="leftbar  nav nav-pills nav-stacked">
-                        <li><a href="#home" class="whiteColor"><i class="fa fa-dashboard fa-fw"></i>Dashboard</a></li><br/>
-                        <li><a href="register" class="whiteColor"><i class="fa fa-hospital-o fa-fw"></i>Hospital</a></li><br/>
-                        <li><a href="#registeredHospital" class="whiteColor"><i class="fa fa-stethoscope fa-fw"></i>Doctor</a></li><br/>
-                        <li><a href="#Update"  class="whiteColor"><i class="fa fa-users fa-fw"></i>Patients</a></li><br/>
-                        <li><a href="#support" class="whiteColor"><i class="fa fa-gear fa-fw"></i>Settings</a></li><br/>
-                        <li><a href="#about" class="whiteColor">About Us</a></li><br/>
+                        <li><a href="#home" style="color: #ffffff;"><i class="fa fa-dashboard fa-fw"></i>Dashboard</a></li><br/>
+                        <li><a href="register" style="color: #ffffff;"><i class="fa fa-hospital-o fa-fw"></i>Hospital</a></li><br/>
+                        <li><a href="#registeredHospital" style="color: #ffffff;"><i class="fa fa-stethoscope fa-fw"></i>Doctor</a></li><br/>
+                        <li><a href="#Update"  style="color: #ffffff;"><i class="fa fa-users fa-fw"></i>Patients</a></li><br/>
+                        <li><a href="#report" style="color: #ffffff;"><i class="fa fa-file-archive-o fa-fw"></i>Reports</a></li><br/>
+                        <li><a href="#support" style="color: #ffffff;"><i class="fa fa-gear fa-fw"></i>Settings</a></li><br/>
                     </ul>
                 </div>
                 <div class="col-md-9" id="top">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h3>Admin dashboard</h3>
+                        <div class="page-header">
+                            <h1 style="font-family: FontAwesome">Admin dashboard</h1>
                         </div>
+                    <!-- .row -->
+                    <div class="row">
+                        <!-- registered hospital-->
+                        <div class="col-lg-4 col-md-6">
+                            <div class="panel panel-primary">
+                                <div class="panel-heading">
+                                    <div class="row">
+                                        <div class="col-xs-3">
+                                            <i class="fa fa-hospital-o fa-4x"></i>
+                                        </div>
+                                        <div class="col-xs-9 text-right">
+                                            <div class="huge">26</div>
+                                            <div>Hospitals registered</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#registeredHospital">
+                                    <div class="panel-footer">
+                                        <span class="pull-left">View Details</span>
+                                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div> <!-- /.end of registered hospital column-->
+
+                        <!-- . reports -->
+                        <div class="col-lg-4 col-md-6">
+                            <div class="panel panel-green">
+                                <div class="panel-heading">
+                                    <div class="row">
+                                        <div class="col-xs-3">
+                                            <i class="fa fa-file-archive-o fa-5x"></i>
+                                        </div>
+                                        <div class="col-xs-9 text-right">
+                                            <div class="huge">12</div>
+                                            <div>Reports</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#reports">
+                                    <div class="panel-footer">
+                                        <span class="pull-left">View Details</span>
+                                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <!--/.end of report column -->
+
+                        <!--doctor's list-->
+                        <div class="col-lg-4 col-md-6">
+                            <div class="panel panel-yellow">
+                                <div class="panel-heading">
+                                    <div class="row">
+                                        <div class="col-xs-3">
+                                            <i class="fa fa-user-md fa-5x"></i>
+                                        </div>
+                                        <div class="col-xs-9 text-right">
+                                            <div class="huge">124</div>
+                                            <div>Doctors</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="#">
+                                    <div class="panel-footer">
+                                        <span class="pull-left">View Details</span>
+                                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <!--/end of doctor's list-->
                     </div>
                 </div>
             </div>
--- resources/views/patients/notification.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: dmjema
- * Date: 5/29/17
- * Time: 10:28 AM
- */
