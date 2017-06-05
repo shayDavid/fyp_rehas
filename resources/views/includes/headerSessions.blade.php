9da08ba916553be721aae65ebc3f29f36023d92e
gderick-1 <godsonderick@gmail.com>
2017-06-05 15:40:41 +0300

    header section modified

--- resources/views/includes/headerSessions.blade.php
+++ resources/views/includes/headerSessions.blade.php
@@ -15,7 +15,7 @@
             {{--side bar links--}}
             <ul class="nav nav-pills nav-stacked">
                 {{--<li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="javascript:ajaxLoad('{{'/'}}')"><i class="fa fa-home fa-fw"></i>Home</a></li><br/>--}}
-                <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="javascript:ajaxLoad('{{'/create'}}')"><i class="fa fa-file-text-o fa-fw {{--fa-2x--}}"></i>eReferrals</a></li><br/>
+                <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="javascript:ajaxLoad('{{'/create'}}')"><i class="fa fa-file-text-o fa-fw {{--fa-2x--}}"></i>Create Referral</a></li><br/>
                 <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="javascript:ajaxLoad('{{'view'}}')" ><i class="fa fa-street-view fa-fw"></i>View referrals</a><br/>
                 <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="admin"><i class="fa fa-adn fa-fw"></i>Admin</a></li><br/>
                 <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="#about"><i class="fa fa-address-book-o fa-fw"></i>About Us</a></li><br/>
