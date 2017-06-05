8f0c840d010820e1460dac0be5d497a97424c6c6
gderick-1 <godsonderick@gmail.com>
2017-06-05 22:15:13 +0300

    footer modified

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,8 +1,6 @@
 
 <script src="{{URL::to('js/bootstrap/jquery-3.2.1.min.js')}}"></script>
 <script src="{{URL::to('js/bootstrap/bootstrap.min.js')}}"></script>
-{{--<script src="{{URL::to('js/bootstrap/jquery.ba-hashchange.min.js')}}"></script>--}}
-<script src="{{URL::to('js/bootstrap/dynamicPage.js')}}"></script>
 <script src="{{URL::to('node_modules/bootstrap-ui/bower_components/dist/js/bootstrap-ui.min.js')}}"></script>
 <footer>
     <h3 style="text-align: center; font-size: medium;">Copyright &copy; 2017 ReHAS Team. All rights reserved.</h3>
