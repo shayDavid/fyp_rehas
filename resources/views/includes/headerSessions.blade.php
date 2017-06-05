72adebacdbb20b1855528afd61d47221f6237d30
gderick-1 <godsonderick@gmail.com>
2017-06-05 22:11:26 +0300

    changes in header section

--- resources/views/includes/headerSessions.blade.php
+++ resources/views/includes/headerSessions.blade.php
@@ -15,8 +15,8 @@
             {{--side bar links--}}
             <ul class="nav nav-pills nav-stacked">
                 {{--<li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="javascript:ajaxLoad('{{'/'}}')"><i class="fa fa-home fa-fw"></i>Home</a></li><br/>--}}
-                <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="javascript:ajaxLoad('{{'/create'}}')"><i class="fa fa-file-text-o fa-fw {{--fa-2x--}}"></i>Create Referral</a></li><br/>
-                <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="javascript:ajaxLoad('{{'view'}}')" ><i class="fa fa-street-view fa-fw"></i>View referrals</a><br/>
+                <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="create"><i class="fa fa-file-text-o fa-fw"></i>Create Referral</a></li><br/>
+                <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="view" ><i class="fa fa-street-view fa-fw"></i>View referrals</a><br/>
                 <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="#setting"><i class="fa fa-gear fa-fw"></i>Settings</a></li><br/>
                 <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="#about"><i class="fa fa-fw"></i></a></li><br/>
             </ul>
--- resources/views/non-referral/index.blade.php
+++ resources/views/non-referral/index.blade.php
@@ -10,17 +10,6 @@
             @include('includes.headerSessions')
             <div class="content container">
                 <div class="row">
-                    {{--a section with carousel --}}
-                    {{-- <section class="col-md-9" id="top">
-                         <div class="carousel slide" data-ride="carousel">
-                             <div class="carousel-inner">
-                                 <div class="item active"><img src="{{URL::to('images/emergency.jpeg')}}" alt="emergency"></div>
-                                 <div class="item"><img src="{{URL::to('images/bloodPressure.jpeg')}}" alt="check blood pressure"></div>
-                                 <div class="item"><img src="{{URL::to('images/green.jpeg')}}" alt="green card"></div>
-                                 <div class="item"><img src="{{URL::to('images/hope.jpeg')}}" alt="patients with hope"></div>
-                             </div>
-                         </div>
-                     </section>--}}
                     <section class="col-md-9" id="top" >
                         <div class="panel panel-default">
 
@@ -36,9 +25,6 @@
                                     <li id="inLine"><p>Doctor can generate report whenever necessary.</p></li>
                                 </ul>
                             </div>
-                            {{--<div class="panel-footer" style="text-align: center">--}}
-                            {{--<h3 class="panel-title">Copyright &copy; 2017 ReHAS Team. All rights reserved.</h3>--}}
-                            {{--</div>--}}
                         </div>
                     </section>
                 </div>
@@ -46,6 +32,5 @@
             </div>
         </div>
     </div>
-
 @endsection
 
