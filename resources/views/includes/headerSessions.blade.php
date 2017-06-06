23e3f06102f7bd282db62022ef773a1ce380c772
gderick-1 <godsonderick@gmail.com>
2017-06-06 08:36:00 +0300

    modified header section

--- resources/views/includes/headerSessions.blade.php
+++ resources/views/includes/headerSessions.blade.php
@@ -14,7 +14,7 @@
         <div>
             {{--side bar links--}}
             <ul class="nav nav-pills nav-stacked">
-                {{--<li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="javascript:ajaxLoad('{{'/'}}')"><i class="fa fa-home fa-fw"></i>Home</a></li><br/>--}}
+                <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="{{route('/')}}"><i class="fa fa-home fa-fw"></i>Home</a></li><br/>
                 <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="create"><i class="fa fa-file-text-o fa-fw"></i>Create Referral</a></li><br/>
                 <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="view" ><i class="fa fa-street-view fa-fw"></i>View referrals</a><br/>
                 <li><a class="small text-uppercase text-letterspacing" style="color: #ffffff;" href="#setting"><i class="fa fa-gear fa-fw"></i>Settings</a></li><br/>
