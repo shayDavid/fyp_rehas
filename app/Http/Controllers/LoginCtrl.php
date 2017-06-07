7c471768f9ad3dee12aa2ca4a8fe2f51f49d9a60
shayDavid <shaydavid46@gmail.com>
2017-06-07 14:01:55 +0300

    still fail message does not work

--- app/Http/Controllers/LoginCtrl.php
+++ app/Http/Controllers/LoginCtrl.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use DB;
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Auth;
 
 class LoginCtrl extends Controller
@@ -19,7 +18,7 @@ class LoginCtrl extends Controller
 
     public function postLogin(Request $request) {
         if(!Auth::attempt(['loginID' => $request['username'], 'password' => $request['password']])) {
-            return redirect()->back()->with(['fail' => 'Wrong Username or Password!']);
+            return redirect()->back()->with(['fail' => 'Wrong Username or Password']);
         } else {
             $title = DB::table('h_i_slogins')->where('loginID', $request['username'])->value('title');
             if($title == "NRP") {
