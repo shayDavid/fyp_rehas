02ac00a3cd47f603049c657843a5573d39f2f42f
gderick-1 <godsonderick@gmail.com>
2017-06-07 16:54:44 +0300

    referral modifications

--- resources/views/referral/receivedReferrals.blade.php
+++ resources/views/referral/receivedReferrals.blade.php
@@ -1,14 +1,48 @@
-@extends('layouts.master')
+@extends('layouts.rpMaster')
+@section('header')
+    <div id="wrapper">
+        <nav class="navbar navbar-default" role="navigation" style="margin-bottom: 0;">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{route('rp')}}" style="color: #FFFFFF; padding-left:
+            50px; font-family: FontAwesome; font-size: x-large">ReHAS</a>
+            </div>
+            <!-- /.navbar-header -->
+            <ul class="nav navbar-top-links navbar-right">
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#" title="ereferral">
+                        <i class="fa fa-envelope fa-fw fa-2x"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <!-- /.dropdown-messages -->
+                </li>
 
-@section('title')
-    ReHAS
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw fa-2x"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+                <!-- /.dropdown -->
+            </ul>
+        </nav>
+    </div>
 @endsection
-
-@section('content')
+@section('main')
     <div class="container">
         <div class="row">
             <div class="leftbar col-md-3" id="sidebar">
-                <ul class="leftbar nav nav-pills nav-stacked" role="menubar">
+                <ul>
                     @include('includes.sidebarRp')
                 </ul>
             </div>
