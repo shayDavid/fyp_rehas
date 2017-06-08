67996a45bd927b883a1e8a0de6794b534eb30a33
gderick-1 <godsonderick@gmail.com>
2017-06-08 23:52:58 +0300

    welcome msg

--- resources/views/non-referral/index.blade.php
+++ resources/views/non-referral/index.blade.php
@@ -16,12 +16,12 @@
                             <p>You can access ReHAS functionalities by following appropriate links as follows</p>
                             <hr>
 
-                            <strong>View Referral</strong>
+                            <strong>Create Referral</strong>
                             <hr>
-                            <p>This functionality allows a doctor, here referred as "Referral Practitioner" to
+                            <p>This functionality allows a doctor, here referred as "Non-Referral Practitioner" to
                             <ul>
-                                <li class="info">View all referral received from different hospitals</li><hr>
-                                <li class="info">Review referral reasons, diagnosis and recommendations</li><hr>
+                                <li class="info">Search patient by using patient identity as registered in HIS</li><hr>
+                                <li class="info">If patient is registered, System display referral form</li><hr>
                                 <li class="info">Deciding on whether to accept or deny the referral</li><hr>
                                 <li class="info">If Doctor accept a referral, Appointment should be put in place and updating the status of the referral</li><hr>
                                 <li class="info">Else if, Referral is denied, Doctor will be redirected back to view page</li><hr>
