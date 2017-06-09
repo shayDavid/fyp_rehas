b2d0a17d8ea2d41f8757bae5b9726f968f0db455
shayDavid <shaydavid46@gmail.com>
2017-06-09 11:04:27 +0300

    Fire event to change status of the referral

--- app/Http/Controllers/SetAppointmentCtrl.php
+++ app/Http/Controllers/SetAppointmentCtrl.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Appointment;
+use App\Events\SetAppointment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Event;
 use DB;
 
 class SetAppointmentCtrl extends Controller {
@@ -17,7 +19,7 @@ class SetAppointmentCtrl extends Controller {
         $aidVar1 = date("my");
         $aid="RA-".$aidVar1."-".$aidVar;
 
-        $referralIDExist = DB::table('appointments')->where('referralID', $request['rid']);
+        $referralIDExist = DB::table('appointments')->where('referralID', $request['rid'])->get();
 
         if(count($referralIDExist) > 0) {
             $request->session()->flash('fail', 'Appointment for this referral has been set already!');
@@ -29,6 +31,9 @@ class SetAppointmentCtrl extends Controller {
         $aidOb->appointmentdate = $request['date'];
         $aidOb->referralID = $request['rid'];
         $aidOb->save();
+
+        $refId = $request['rid'];
+        Event::fire(new SetAppointment($refId));
 
         $request->session()->flash('success', 'Appointment has been set successfully!');
         return redirect()->route('referral_view');
